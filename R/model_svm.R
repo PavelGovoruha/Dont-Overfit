75dbd0f77cdc59b167692e8d8e5dba3d1cfa3f95
Pavel Govoruha <govoruha94@gmail.com>
2019-04-20 00:39:49 +0300

    ***

---
+++ R/model_svm.R
@@ -0,0 +1,8 @@
+library(tidyverse)
+library(kernlab)
+library(fastDummies)
+
+train <- read_rds('results/new_train.rds')
+test <- read_rds('results/new_test.rds')
+submission <-read_csv('data/sample_submission.csv') 
+
