1caf87354f887a2fe5a082892741fee7228c2364
Pavel Govoruha <govoruha94@gmail.com>
2019-04-24 00:25:29 +0300

    ***

--- R/caret_models.R
+++ R/caret_models.R
@@ -12,11 +12,11 @@ control.glm <- trainControl(method = 'boot', number = 100,savePredictions = 'fin
                             classProbs = TRUE, summaryFunction = twoClassSummary, 
                             returnResamp = 'final', allowParallel = TRUE)
 
-tune.glm <- expand.grid(lambda = seq(from = 0, to = 0.2, by = 0.01), alpha = c(0,1))
+tune.glm <- expand.grid(lambda = seq(from = 0, to = 2, by = 0.01), alpha = 0)
 
 plan(multiprocess)
 time1 <- Sys.time()
-glmnet_model <- train(target ~ ., method = 'glmnet', metric = 'ROC', data = train[,-1], 
+glmnet_model <- train(target ~ ., method = 'glmnet', metric = 'ROC', data = train_rf[,-1], 
                       trControl = control.glm, tuneGrid = tune.glm)
 Sys.time() - time1
 
@@ -26,28 +26,94 @@ pred_test_raw <- predict(glmnet_model, test, type = 'raw')
 pred_test_prob <- predict(glmnet_model, test, type = 'prob')
 submission$target <- pred_test_prob$Y
 write_csv(submission, 'data/caret_glm.csv')
-#cforest model
 
-#glmnet model
-control.cf <- trainControl(method = 'boot', number = 100, savePredictions = 'final',
-                            classProbs = TRUE, summaryFunction = twoClassSummary, 
-                            returnResamp = 'final', allowParallel = TRUE)
+#glmnet auc score: 0.8426
 
-tune.cf <- expand.grid(mtry = 8:10)
-
+#bayesian glm
+bayes_glm.control <- trainControl(method = 'boot', number = 100,savePredictions = 'final',
+                                         classProbs = TRUE, summaryFunction = twoClassSummary, 
+                                         returnResamp = 'final', allowParallel = TRUE)
 plan(multiprocess)
 time1 <- Sys.time()
-cforest_model <- train(target ~ ., method = 'cforest', metric = 'ROC', data = train[,-1], 
-                      trControl = control.cf, tuneGrid = tune.cf)
+bayes_glm_model <- train(target ~ ., method = 'bayesglm', metric = 'ROC', data = train_rf[,-1], 
+                       trControl = bayes_glm.control)
 Sys.time() - time1
-plot(cforest_model)
-cforest_model
-pred_test_raw <- predict(cforest_model, test, type = 'raw')
-pred_test_raw[1:10]
-pred_test_prob <- predict(cforest_model, test, type = 'prob')
-pred_test_prob[1:20,]
-submission$target <- pred_test_prob$Y
-write_csv(submission, 'results/caret_cforest.csv')
+bayes_glm_model
 
+#bayesglm auc score: 0.84
 
-#glmnet boot ROC: 0.7574331, cforest boot ROC: 0.7134685
+#generalised additive models with splines
+gener_spline.control <- trainControl(method = 'boot', number = 25,savePredictions = 'final',
+                                     classProbs = TRUE, summaryFunction = twoClassSummary, 
+                                     returnResamp = 'final', allowParallel = TRUE)
+
+gener_spline.tune <- expand.grid(df = 1)
+plan(multiprocess)
+time1 <- Sys.time()
+gener_spline_model <- train(target ~ ., method = 'gamSpline', metric = 'ROC', data = train_rf[,-1], 
+                         trControl = gener_spline.control, tuneGrid = gener_spline.tune)
+Sys.time() - time1
+gener_spline_model
+
+plot(gener_spline_model)
+
+#generalised additive models with splines auc score: 0.823
+
+#Radial SVM
+kern.control <- trainControl(method = 'boot', number = 100,savePredictions = 'final',
+                             classProbs = TRUE, summaryFunction = twoClassSummary, 
+                             returnResamp = 'final', allowParallel = TRUE)
+kern.tune <- expand.grid(sigma = 10^(-5:-1), C = 10^(-3:3), Weight = c("Y" = 0.36, "N" = 0.64))
+plan(multiprocess)
+time1 <- Sys.time()
+kern_model <- train(target ~ ., method = 'svmRadialWeights', metric = 'ROC', data = train_rf[,-1],
+                    trControl = kern.control, tuneGrid = kern.tune)
+Sys.time() - time1
+kern_model
+plot(kern_model)
+
+#svmRadial auc score: 0.833
+
+#Next time: 
+#1. Ensembles of Generalized Linear Models
+#2. Boosted Generalized Linear Model
+#3. Distance Weighted Discrimination with Radial Basis Function Kernel
+#4. Penalized Discriminant Analysis
+#5. ROC-Based Classifier
+#6. Generalised additive models with splines
+
+#Predict with baysglm
+pred_test_bayes <- predict(bayes_glm_model, test_rf[,-1], type = 'raw')
+pred_test_bayes[1:10]
+pred_test_bayes_probs <- predict(bayes_glm_model, test_rf[,-1], type = 'prob')
+pred_test_bayes_probs[1:10,]
+submission$target <- pred_test_bayes_probs$Y
+write_csv(submission, 'results/bayesglm_caret.csv')
+
+#Predict by glmnet
+pred_test_raw_glmnet <- predict(glmnet_model, test_rf[,-1], type = 'raw')
+pred_test_raw_glmnet[1:10]
+pred_test_prob_glmnet <- predict(glmnet_model, test_rf[,-1], type = 'prob')
+pred_test_prob_glmnet[1:10,]
+submission$target <- pred_test_prob_glmnet$Y
+write_csv(submission, 'results/caret_glmnet.csv')
+
+#Predict with kern_model
+pred_test_kern_raw <- predict(kern_model, test_rf[,-1], type = 'raw')
+pred_test_kern_raw[1:10]
+pred_test_kern_prob <- predict(kern_model, test_rf[,-1], type = 'prob')
+pred_test_kern_prob[1:10,]
+submission <- read_csv('data/sample_submission.csv')
+submission$target <- pred_test_kern_prob$Y
+write_csv(submission, 'results/svm_caret.csv')
+
+#Predict with spline_model
+pred_test_spline_raw <- predict(gener_spline_model, test_rf[,-1], type = 'raw')
+pred_test_spline_raw[1:10]
+pred_test_spline_prob <- predict(gener_spline_model, test_rf[,-1], type = 'prob')
+pred_test_spline_prob[1:10,]
+submission <- read_csv('data/sample_submission.csv')
+submission$target <- pred_test_spline_prob$Y
+write_csv(submission, 'results/spline_caret.csv')
+
+#Basic ensembling
